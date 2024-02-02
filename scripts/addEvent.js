601e57353d659ac53dfe65e13575cd20c5b835b1
Shruti <52789421+shrued@users.noreply.github.com>
2024-02-02 11:11:31 -0500

    Update addEvent.js

--- scripts/addEvent.js
+++ scripts/addEvent.js
@@ -7,12 +7,15 @@ window.onload = function() {
 
 function myFunction() {
     document.getElementById('demo').textContent = 'Moused over!';
+    return () => document.getElementById('demo').textContent = msg;
 }
 
 function mySecondFunction() {
     document.getElementById('demo').textContent = 'Clicked!';
+    return () => document.getElementById('demo').textContent = msg;
 }
 
 function myThirdFunction() {
     document.getElementById('demo').textContent = 'Moused out!';
+    return () => document.getElementById('demo').textContent = msg;
 }
